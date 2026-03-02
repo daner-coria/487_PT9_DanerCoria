d1fbf5e40b1108631d112ba96e3e167f298b7705
daner-coria <danereidance@gmail.com>
2026-03-02 12:06:25 +0100

    Update main.java
    2a modificació

--- PT9_DanerCoria/src/main.java
+++ PT9_DanerCoria/src/main.java
@@ -2,5 +2,6 @@ public class main {
 	public static void main (String [] args) {
 		System.out.println("Hola món");
 		System.out.println("primera modificació git");
+		System.out.println("segona modificació git");
 	}
 }
