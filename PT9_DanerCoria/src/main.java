238d526bf19bfa6a7dae24bd5684923f56ef8517
daner-coria <danereidance@gmail.com>
2026-03-03 12:43:46 +0100

    3a modificació

--- PT9_DanerCoria/src/main.java
+++ PT9_DanerCoria/src/main.java
@@ -1,6 +1,8 @@
 public class main {
 	public static void main (String [] args) {
 		System.out.println("Hola món");
-		System.out.println("2a modificació GitHub");	
+		System.out.println("primera modificació git");
+		System.out.println("2a modificació GitHub");
+        System.out.println("3a modificació Git");
 	}
 }
